9a34adafad92b784ff6029666f5fa5e06920b22e
Jana Vydrová <v.janca@gmail.com>
2020-01-20 18:33:37 +0100

    lightbox, disable buttons, toggler menu

--- html/konference/main.js
+++ html/konference/main.js
@@ -1,3 +1,11 @@
+//iframe lightbox
+
+[].forEach.call(document.getElementsByClassName("iframe-lightbox-link"), function(el) {
+    el.lightbox = new IframeLightbox(el);
+});
+
+//Map
+
 var center = SMap.Coords.fromWGS84(16.5771161, 49.2181472);
 var m = new SMap(JAK.gel("mapa"), center, 16);
 m.addDefaultLayer(SMap.DEF_BASE).enable();
@@ -10,6 +18,24 @@ layer.enable();
 var options = {};
 var marker = new SMap.Marker(center, "myMarker", options);
 layer.addMarker(marker);
+
+//menu
+
+let menuBtn = document.querySelector(".navbar-toggler");
+let menuSapce = document.getElementById("collapse_target");
+
+document.querySelectorAll(".nav-link").forEach(item => {
+    item.addEventListener("click", () => {
+        if (window.innerWidth <= 767) {
+            menuBtn.classList.add("collapsed");
+            menuBtn.setAttribute("aria-expanded", "false");
+            menuSapce.classList.remove("show");
+        }
+    });
+});
+
+
+//To top arrow
 
 let arrowToTop = document.getElementById("to-top");
 let menu = document.querySelector("header");
